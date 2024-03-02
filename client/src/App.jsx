9588df51385f3596d82339cae8912e12e8b707dd
Annath Beevi <annathbeevi01@gmail.com>
2024-03-02 06:42:46 +0530

    file upload design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
+import { Button, FileInput, Label } from 'flowbite-react'
 import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <div className="p-3 max-w-3xl mx-auto min-h-screen my-20">
+        <h1 className='text-3xl font-bold lg:text-4xl text-center text-blue-950'>File Upload</h1>
+        <form className='flex flex-col justify-center mt-20'>
+          <FileInput type='file' accept='pdf/*'/>
+          <Button gradientDuoTone="cyanToBlue" outline className='mt-4 max-w-32 sm:ml-80 ml-40'>Upload</Button>
+        </form>
+      </div>
+    </div>
   )
 }
 
